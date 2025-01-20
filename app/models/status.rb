ee49cbd492c8cab5611317f789de6e0daa7ecf28
Claire <claire.github-309c@sitedethib.com>
2025-01-20 16:50:33 +0100

    Add `quote_approval_policy` to Status

--- app/models/status.rb
+++ app/models/status.rb
@@ -44,6 +44,13 @@ class Status < ApplicationRecord
 
   MEDIA_ATTACHMENTS_LIMIT = 4
 
+  QUOTE_APPROVAL_POLICY_FLAGS = {
+    public: (1 << 0),
+    followers: (1 << 1),
+    followed: (1 << 2),
+    unknown: (1 << 3),
+  }.freeze
+
   rate_limit by: :account, family: :statuses
 
   self.discard_column = :deleted_at
